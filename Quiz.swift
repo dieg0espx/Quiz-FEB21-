98b743d48c2135e4549fab1802f574cce529fd98
dieg0espx <55413117+dieg0espx@users.noreply.github.com>
2020-02-21 20:28:03 -0800

    Add files via upload

--- Quiz.swift
+++ Quiz.swift
@@ -1,66 +1,60 @@
 import Foundation
 
-func printing(){
-    //let n = (readLine()
-    //print(n)
-    //printing()
-    
-
+func Quiz(){
     var arrayTree: [String] = []
     var whatIhave = "1"
-    var size = Int(readLine()!)!
+    let size = Int(readLine()!)!
     
-    
-    
-    for i in 0..<size{
+    for _ in 0..<size{
         arrayTree.append("")
     }
- 
     
-   // for _ in 0..<size-1 {
-        var read = String(readLine()!)
+    for _ in 0..<size{
+        let read = String(readLine()!)
         let spliting = read.components(separatedBy: " ")
         let num1 = String(spliting[0])
         let num2 = String(spliting[1])
-    
-    var num1Int = Int(num1)
-    var num2Int = Int(num2)
-    
-    
-    for _ in 0..<size{
-    if (whatIhave.contains(num1)){
-        var variable = arrayTree[num1Int!] + num2
-        arrayTree[num1Int! - 1] = variable
-    }
-    else if (whatIhave.contains(num2)){
-        var variable = arrayTree[num2Int!] + num1
-               arrayTree[num2Int! - 1] = variable
+      
+        
+        let num1Int = Int(num1)
+        let num2Int = Int(num2)
+        
+        var v = ""
+        
+        if (whatIhave.contains(num1)){
+            v = String(arrayTree[num1Int!-1]) + num2
+            arrayTree[num1Int!-1] = v
+            whatIhave.append(num2)
+            
         }
-    }
-    
-        print(arrayTree)
-        
-        
-    
-
+        else if (whatIhave.contains(num2)){
+            v = String(arrayTree[num2Int!-1]) + num1
+            arrayTree[num2Int!-1] = v
+            whatIhave.append(num1)
+        }
     
     
     
-    
-    //let tree = [[4,6],[L,R],[R, 5],[2,7],[nil,nil],[nil, 3],[nil,nil]]
-    let tree = ["4,6", "L,R", "L,5", "2,7", "L,R", "L,3", "L,R"]
     var num = 1
     var numString = ""
+      var sol = ""
     
-    
-    while(num <= tree.count){
-        for i in 0..<tree.count{
-            if tree[i].contains(numString){
-                print(i+1)
+    while(num <= arrayTree.count){
+        for i in 0..<arrayTree.count{
+            if arrayTree[i].contains(numString){
+                let result = (i+1)
+                let resultString = String(result)
+                sol.append(resultString)
             }
         }
         num += 1
         numString = String(num)
+        }
+        //if(sol.count == size-1){
+            print(sol)
+        //}
     }
-    
 }
+    
+
+
